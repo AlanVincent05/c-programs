9dfa91904129fe0b04446501767c61cb2cd09f60
Alan Vincent <alt4alan@gmail.com>
2025-02-16 21:19:15 +0530

    Update switch.c

--- switch.c
+++ switch.c
@@ -1,121 +1,109 @@
-#include<stdio.h>
-//Insert an element
-int insert(int *a)
-{
-        int pos,i,num;
-	int n=sizeof(a)/sizeof(a[0]);
-	printf("Enter the position of the element to be inserted: ");
-        scanf("%d",&pos);
-        pos=pos-1;
-        printf("Enter the element to be inserted: ");
-        scanf("%d",&num);
+#include <stdio.h>
 
-        for(i=n;i>=pos;i--)
-        {
-                a[i+1]=a[i];
-        }
-        a[pos]=num;
-        for(i=0;i<=n;i++)
-        {
-                printf("%d\t",a[i]);
-        }
-        printf("\n");
-        return 0;
+void printArray(int arr[], int n) {
+    int i;
+    for(i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
 
-
-//Delete an element
-int del(int *a)
-{
-        int i,pos;
-	int n=sizeof(a)/sizeof(a[0]);
-	printf("Enter the position of the element to be deleted: ");
-        scanf("%d",&pos);
-        pos=pos-1;
-
-        for(i=pos;i<n;i++)
-        {
-                a[i]=a[i+1];
-        }
-        for(i=0;i<=n-1;i++)
-        {
-                printf("%d\t",a[i]);
-        }
-        printf("\n");
-        return 0;
+int insertElement(int arr[], int n, int pos, int elem) {
+    if(pos < 0 || pos > n) {
+        printf("Invalid position!\n");
+        return n;
+    }
+    int i;
+    for(i = n; i > pos; i--){
+        arr[i] = arr[i - 1];
+    }
+    arr[pos] = elem;
+    return n + 1;
 }
 
-
-//Print the array
-int traverse(int *a)
-{
-        int i;
-	int n=sizeof(a)/sizeof(a[0]);
-        for(i=0;i<n;i++)
-        {
-                printf("%d\t",a[i]);
-        }
-        printf("\n");
-        return 0;
+int deleteElement(int arr[], int n, int pos) {
+    if(pos < 0 || pos >= n) {
+        printf("Invalid position!\n");
+        return n;
+    }
+    int i;
+    for(i = pos; i < n - 1; i++){
+        arr[i] = arr[i + 1];
+    }
+    return n - 1;
 }
 
-
-//Merging two arrays
-int merge(int *a)
-{
-	int num_a=sizeof(a)/sizeof(a[0]);
-        int b[100],c[100],i,num_b;
-        printf("Enter the number of elements to be entered: ");
-        scanf("%d",&num_b);
-        for(i=0;i<num_b;i++)
-	{
-                scanf("%d",&b[i]);
-        }
-	for(i=0;i<num_a;i++)
-	{
-		c[i]=a[i];
-	}
-	for(i=num_a;i<(num_a+num_b);i++)
-	{
-		c[i]=b[i-num_a];
-	}
-	printf("\n");
-	for(i=0;i<(num_a+num_b);i++)
-	{
-	printf("%d\n",c[i]);
-	}
-	return 0;
+int mergeArrays(int arr1[], int n1, int arr2[], int n2) {
+    int i;
+    for(i = 0; i < n2; i++){
+        arr1[n1 + i] = arr2[i];
+    }
+    return n1 + n2;
 }
 
-//Enter the array
-int main()
-{
-        int a[100],i,n,op;
-        printf("Enter the number of elements: ");
-        scanf("%d",&n);
-        for(i=0;i<n;i++)
-        {
-                scanf("%d",&a[i]);
+int main() {
+    int arr[100];
+    int n, choice, pos, elem, n2, i;
+    
+    printf("Enter initial number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements: ", n);
+    for(i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    do {
+        printf("\nMenu:\n");
+        printf("1. Insert an element\n");
+        printf("2. Delete an element\n");
+        printf("3. Merge with another array\n");
+        printf("4. Print the array\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        
+        switch(choice) {
+            case 1:
+                printf("Enter position to insert (0-indexed): ");
+                scanf("%d", &pos);
+                printf("Enter element to insert: ");
+                scanf("%d", &elem);
+                n = insertElement(arr, n, pos, elem);
+                printf("Array after insertion: ");
+                printArray(arr, n);
+                break;
+            case 2:
+                printf("Enter position to delete (0-indexed): ");
+                scanf("%d", &pos);
+                n = deleteElement(arr, n, pos);
+                printf("Array after deletion: ");
+                printArray(arr, n);
+                break;
+            case 3: {
+                int arr2[100];
+                printf("Enter number of elements in second array: ");
+                scanf("%d", &n2);
+                printf("Enter %d elements: ", n2);
+                for(i = 0; i < n2; i++){
+                    scanf("%d", &arr2[i]);
+                }
+                n = mergeArrays(arr, n, arr2, n2);
+                printf("Array after merging: ");
+                printArray(arr, n);
+                break;
+            }
+            case 4:
+                printf("Current array: ");
+                printArray(arr, n);
+                break;
+            case 5:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
         }
-	do
-	{
-		printf("1 for insert\n2 for delete\n3 for traverse\n4 for merge\n5 to exit\nEnter the operation: ");
-		scanf("%d",&op);
-		switch(op)
-		{
-			case 1:
-				insert(a);
-				break;
-			case 2:
-				del(a);
-				break;
-			case 3:
-				traverse(a);
-				break;
-			case 4:
-				merge(a);
-				break;
-		}
-	}while(op<5);
-	return 0;
+        
+    } while(choice != 5);
+    
+    return 0;
 }
