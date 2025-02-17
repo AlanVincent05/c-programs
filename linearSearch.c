d8f9665047a0b9a17ff7ead810c616b7779a455a
Alan Vincent <alt4alan@gmail.com>
2025-02-17 21:16:30 +0530

    Update linearSearch.c

--- linearSearch.c
+++ linearSearch.c
@@ -1,35 +1,42 @@
-#include<stdio.h>
-int linearSearch(int *arr, int n, int key)
-{
-	for(int i=0;i<n;i++)
-	{
-		if(arr[i]==key)
-		{
-			return i;
-		}
-	}
-	return -1;
+#include <stdio.h>
+
+void linearSearch(int arr[], int n, int search) {
+    int count = 0;
+    printf("Element %d found at positions: ", search);
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == search) {
+            // Print position as i+1 (not the array index)
+            printf("%d ", i + 1);
+            count++;
+        }
+    }
+    if (count == 0) {
+        printf("Not found");
+    }
+    printf("\nTotal Occurrences: %d\n", count);
 }
 
-int main()
-{
-	int n,j,key,arr[100];
-        printf("Enter the number of elements: ");
-        scanf("%d",&n);
-        for(j=0;j<n;j++)
-        {
-                scanf("%d",&arr[j]);
-	}
-        printf("Enter the element to be found: ");
-        scanf("%d",&key);
-	int i=linearSearch(arr,n,key);
-	if(i==-1)
-	{
-		printf("Key not found\n");
-	}
-	else
-	{
-		printf("Key found at position: %d\n",i+1);
-	}
-	return 0;
+int main() {
+    int n, search;
+    
+    // Input array size
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    
+    int arr[n];  // Using a variable length array (C99 feature)
+    
+    // Input array elements
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    // Input the element to search
+    printf("Enter the element to search: ");
+    scanf("%d", &search);
+    
+    // Call the linear search function
+    linearSearch(arr, n, search);
+    
+    return 0;
 }
